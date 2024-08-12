cd95d691931111d2115da01aac1ede62d4a4cb24
itsdxrk <tanzeemr2@gmail.com>
2024-08-12 19:26:37 +0100

    Rework profile picture in UserPanel

--- src/main/java/me/itsdxrk/srcjclient/gui/wrframe/UserPanel.java
+++ src/main/java/me/itsdxrk/srcjclient/gui/wrframe/UserPanel.java
@@ -8,11 +8,10 @@ import me.itsdxrk.srcjclient.models.UserID;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.BufferedOutputStream;
 import java.util.ArrayList;
-
-import static me.itsdxrk.srcjclient.gui.searchframe.GamePanel.wrHolders;
 public class UserPanel extends JPanel {
+
+    public static JLabel timeLabel = new JLabel("", SwingConstants.LEFT);
 
     SRCApi api = new SRCApi();
     public UserPanel() {
@@ -27,20 +26,25 @@ public class UserPanel extends JPanel {
     }
 
     private void addComponents() {
-        JLabel pfpLabel = new JLabel();
+        int loc = 0;
         JLabel playerNames = new JLabel();
+        JLabel pfpLabel = new JLabel();
         ArrayList<UserID> wrHolders = GamePanel.wrHolders;
         ArrayList<String> wrHolderNames = new ArrayList<>();
         for (UserID user : wrHolders) {
             wrHolderNames.add(user.getName());
         }
-        playerNames.setText(String.join("\n", wrHolderNames));
-        this.add(playerNames, new GridBagConstraints(0, 0, 1, 1, 1, 1, 10, 1, new Insets(0, 0, 0, 0), 0, 0));
+        playerNames.setText(String.join(", ", wrHolderNames));
         if (wrHolders.size() == 1) {
             BufferedImage pfp = api.getUserProfilePic(wrHolders.get(0).getId());
-            pfpLabel.setIcon(new ImageIcon(pfp));
-            this.add(pfpLabel, new GridBagConstraints(0, 1, 1, 1, 1, 1, 10, 1, new Insets(0, 0, 0, 0), 0, 0));
+            if (pfp != null) {
+                pfpLabel.setIcon(new ImageIcon(pfp));
+                this.add(pfpLabel, new GridBagConstraints(0, 0, 1, 1, 2, 1, 10, 1, new Insets(0, 0, 0, 0), 0, 0));
+            }
+            loc++;
         }
+        this.add(playerNames, new GridBagConstraints(0, loc, 1, 1, 1, 1, 10, 1, new Insets(0, 0, 0, 0), 0, 0));
+        this.add(timeLabel, new GridBagConstraints(0, loc+1, 1, 1, 1, 1, 10, 1, new Insets(0, 0, 0, 0), 0, 0));
 
 
 
